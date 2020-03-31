e1c5651e789ffac8204bed2ab4f4e909e9262911
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2020-03-31 17:51:20 +0900

    Revert "fix travis"
    This reverts commit 82ebc2da33ac767446cb5163a6ff333981e9cfda.

--- tests/travis/install-script.sh
+++ tests/travis/install-script.sh
@@ -2,7 +2,6 @@
 
 composer install
 
-case "$TRAVIS_PHP_VERSION" in
-	5\.6) composer require --dev "phpunit/phpunit 5.*" ;;
-	7\.*) composer require --dev "phpunit/phpunit 6.*" ;;
-esac
+if [ "$TRAVIS_PHP_VERSION" = "7.2" ]; then
+	composer require --dev "phpunit/phpunit 6.*"
+fi
